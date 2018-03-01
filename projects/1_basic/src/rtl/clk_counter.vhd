4b426f6e9d52f999ae00d07b8bc892d69cf3184f
filip <filipjasic8@gmail.com>
2018-03-01 03:34:15 +0100

    Realizovati arhitekturu modula clk_counter.vhd - brojač koji od 50 MHz generiše signal frekvencije 1

--- projects/1_basic/src/rtl/clk_counter.vhd
+++ projects/1_basic/src/rtl/clk_counter.vhd
@@ -29,22 +29,22 @@ END clk_counter;
 
 ARCHITECTURE rtl OF clk_counter IS
 
+component reg is
+	generic(
+		WIDTH    : positive := 26;
+		RST_INIT : integer := 0
+	);
+	port(
+		i_clk  : in  std_logic;
+		in_rst : in  std_logic;
+		i_d    : in  std_logic_vector(WIDTH-1 downto 0);
+		o_q    : out std_logic_vector(WIDTH-1 downto 0)
+	);
+end component reg;
+
 SIGNAL   counter_r : STD_LOGIC_VECTOR(25 DOWNTO 0);
 SIGNAL   counter_r_next : STD_LOGIC_VECTOR(25 DOWNTO 0);
-SIGNAL   enable : STD_LOGIC;
-
-COMPONENT reg IS GENERIC(
-                              -- maksimalna vrednost broja do kojeg brojac broji
-                              max_cnt : STD_LOGIC_VECTOR(25 DOWNTO 0) := "10111110101111000010000000" -- 50 000 000
-                             );
-                      PORT   (
-                               clk_i     : IN  STD_LOGIC; -- ulazni takt
-                               rst_i     : IN  STD_LOGIC; -- reset signal
-                               cnt_en_i  : IN  STD_LOGIC; -- signal dozvole brojanja
-                               cnt_rst_i : IN  STD_LOGIC; -- signal resetovanja brojaca (clear signal)
-                               one_sec_o : OUT STD_LOGIC  -- izlaz koji predstavlja proteklu jednu sekundu vremena
-                             );
-END component reg;
+--SIGNAL   enable : STD_LOGIC;
 
 BEGIN
 
@@ -52,17 +52,18 @@ BEGIN
 -- brojac koji kada izbroji dovoljan broj taktova generise SIGNAL one_sec_o koji
 -- predstavlja jednu proteklu sekundu, brojac se nulira nakon toga
 					
-			--		REG1 : reg
-					
-				--	port map(
-					--clk_i => clk_i,
-					--rst_i => rst_i,
-					--i_d => next_cnt,
-					--o_q => counter_r_next
-					--	);
-		
-counter_r_next <= "00000000000000000000000000" when rst_i= '1' else counter_r;
-counter_r_next <= (counter_r + "00000000000000000000000001") when enable = '1' else counter_r;
+REG1 : reg port map(
+	i_clk => clk_i,
+	in_rst => rst_i,
+	i_d => counter_r_next,
+	o_q => counter_r
+);
+		--kad je reset upaljen sve na 0
+		--kada je enablovan i kada je u sledecem brojacu maksimalna vrednost onda dodajem 1 u brojacu
+counter_r_next <= (others=>'0') when cnt_rst_i='1' else
+					(others=>'0') when (cnt_en_i='1' and counter_r_next=max_cnt) else
+					counter_r+1;
 
-		one_sec_o <= '1';
+one_sec_o<='1' when counter_r=max_cnt else '0';
+		
 END rtl;
