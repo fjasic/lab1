4bc82fa6c88038e6bea1a7a52983596cedc2ab84
filip <filipjasic8@gmail.com>
2018-03-01 02:55:42 +0100

    core generator x

--- projects/1_basic/src/rtl/clk_counter.vhd
+++ projects/1_basic/src/rtl/clk_counter.vhd
@@ -30,12 +30,39 @@ END clk_counter;
 ARCHITECTURE rtl OF clk_counter IS
 
 SIGNAL   counter_r : STD_LOGIC_VECTOR(25 DOWNTO 0);
+SIGNAL   counter_r_next : STD_LOGIC_VECTOR(25 DOWNTO 0);
+SIGNAL   enable : STD_LOGIC;
+
+COMPONENT reg IS GENERIC(
+                              -- maksimalna vrednost broja do kojeg brojac broji
+                              max_cnt : STD_LOGIC_VECTOR(25 DOWNTO 0) := "10111110101111000010000000" -- 50 000 000
+                             );
+                      PORT   (
+                               clk_i     : IN  STD_LOGIC; -- ulazni takt
+                               rst_i     : IN  STD_LOGIC; -- reset signal
+                               cnt_en_i  : IN  STD_LOGIC; -- signal dozvole brojanja
+                               cnt_rst_i : IN  STD_LOGIC; -- signal resetovanja brojaca (clear signal)
+                               one_sec_o : OUT STD_LOGIC  -- izlaz koji predstavlja proteklu jednu sekundu vremena
+                             );
+END component reg;
 
 BEGIN
 
 -- DODATI:
 -- brojac koji kada izbroji dovoljan broj taktova generise SIGNAL one_sec_o koji
 -- predstavlja jednu proteklu sekundu, brojac se nulira nakon toga
+					
+			--		REG1 : reg
+					
+				--	port map(
+					--clk_i => clk_i,
+					--rst_i => rst_i,
+					--i_d => next_cnt,
+					--o_q => counter_r_next
+					--	);
+		
+counter_r_next <= "00000000000000000000000000" when rst_i= '1' else counter_r;
+counter_r_next <= (counter_r + "00000000000000000000000001") when enable = '1' else counter_r;
 
-
+		one_sec_o <= '1';
 END rtl;
